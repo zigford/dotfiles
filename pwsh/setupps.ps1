775dbb05b8252aa9194d9ced1a732d173ab2c28c
Jesse Harris <zigford@gmail.com>
2019-09-22 14:42:39 +1000

    added vim submodule ps1 pwsh installer

---
+++ pwsh/setupps.ps1
@@ -0,0 +1,38 @@
+[CmdLetBinding()]
+Param()
+# Using Pwsh 7 features
+
+function New-SymbolicLink {
+    [CmdLetBinding()]
+	Param($Target,$Link)
+
+	If (
+            (Test-Path -Path $Link) -and
+            (Get-Item -Path $Link).PSIsContainer -eq $False
+       ) {
+		Write-Information "File/Link already exists"
+	} else {
+		If ($PSVersionTable.Platform -eq 'Unix') {
+			ln -s $Target $Link
+		} else {
+			If ((Get-Item $Target).PSIsContainer) {
+				cmd.exe /c mklink /D $Link $Target
+			} else {
+				cmd.exe /c mklink $Link $Target
+			}
+		}
+	}
+}
+
+$ProfileRoot = Split-Path -Path $profile -Parent
+$ProfileSource = Join-Path -Path $PSScriptRoot -ChildPath 'Microsoft.PowerShell_profile.ps1'
+If (-Not (Test-Path -Path $ProfileRoot)) {
+    New-Item -ItemType Directory -Path $ProfileRoot
+}
+
+If (-Not (Test-Path -Path $profile)) {
+    Write-Verbose "Linking profile"
+    New-SymbolicLink $ProfileSource $ProfileRoot
+} else {
+    Write-Warning "Profile already exists"
+}
--- vim/vimfiles/ftplugin/python.vim
+++
@@ -1,1 +0,0 @@
-map <F5> :w<CR>:!C:\Python36\python.exe %<CR>
